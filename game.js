2abf36f39c91f8b6d015595f6ec98613a01d4502
kirtigupta2458 <72331388+kirtigupta2458@users.noreply.github.com>
2023-06-15 17:59:41 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,1 @@
-AFRAME.registerComponent("game", {
-    schema: {
-        gameState: { type: "string", default: "play" },
-      },
 
-    init: function () {
-        var duration = 300;
-        var timerEl = document.querySelector("#timer");
-        this.startTimer(duration, timerEl);
-    },
-
-    
-})
